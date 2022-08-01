545cd8158378128adc12e2336b4096a5eea4ad92
Martin C. Doege <mdoege@compuserve.com>
2022-08-01 08:06:10 +0200

    add HD version

--- README.md
+++ README.md
@@ -2,10 +2,17 @@
 
 An orrery in PyGame, based on code from [pico-solar-system](https://github.com/dr-mod/pico-solar-system)
 
-![screenshot](screenshot.png)
+## Usage
 
 Press left and right cursor keys to advance by a single day, up and down cursor keys to advance by 30 days, or page up and down to advance by 365 days.
 Press Return to jump back to today's date; press A to toggle animation.
+
+## Screenshots
+
+There is a version with the original graphics and also an upgraded HD version:
+
+![screenshot](screenshot.png)
+![screenshot](screenshot2.png)
 
 ## License
 
---
+++ orrery_hd.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+
+# High-res Python orrery
+
+import pygame
+import time, math
+import orrp
+
+RES  = 950      # internal resolution
+SRES = 950      # window size
+ANIM_SPEED = 4  # animation speed factor
+
+pic = (
+"sun.png",
+"mercury.png",
+"venus.png",
+"earth.png",
+"mars.png",
+"jupiter.png",
+"saturn.png",
+"uranus.png",
+"neptune.png",
+)
+
+pic2 = []
+for n, p in enumerate(pic):
+	img = pygame.image.load("img/" + p)
+	pic2.append(img)
+
+class Orr:
+	def __init__(s):
+		pygame.init()
+		s.res = SRES, SRES
+		s.screen = pygame.display.set_mode(s.res, pygame.RESIZABLE)
+		pygame.display.set_caption('Orrery')
+		s.clock = pygame.time.Clock()
+		s.dazz = pygame.Surface((RES, RES))
+		s.plusDays = 0
+		s.anim = False
+		s.anim_start = 0
+		s.old_offset = -1e6
+
+	def events(s):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT: s.running = False
+			if event.type == pygame.VIDEORESIZE:
+				s.res = event.w, event.h
+				s.screen = pygame.display.set_mode(s.res, pygame.RESIZABLE)
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+				s.plusDays += 1
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+				s.plusDays -= 1
+
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+				s.plusDays += 30
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+				s.plusDays -= 30
+
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_PAGEUP:
+				s.plusDays += 365
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_PAGEDOWN:
+				s.plusDays -= 365
+
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+				s.plusDays = 0
+
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_a:
+				s.anim = not s.anim
+				if s.anim:
+					s.anim_start = time.time()
+				else:
+					s.plusDays += ANIM_SPEED * (time.time() - s.anim_start)
+
+	def run(s):
+		s.running = True
+		while s.running:
+			s.clock.tick(50)
+			s.events()
+			s.update()
+		pygame.quit()
+
+	def update(s):
+		s.dazz.fill((0, 0, 0))
+		WIDTH, HEIGHT = RES, RES
+		PL_CENTER = (int(WIDTH / 2), int(WIDTH / 2))
+		seconds_absolute = time.time()
+		plusDays = 0
+		if s.anim:
+			ani = ANIM_SPEED * (time.time() - s.anim_start)
+		else:
+			ani = 0
+		offset = s.plusDays + ani
+		ti = time.localtime(seconds_absolute + 86400 * offset)
+		if offset != s.old_offset:
+			s.old_offset = offset
+			o1 = "%04u-%02u-%02u" % (ti[0], ti[1], ti[2])
+			if round(offset) == 0: o2 = ""
+			else: o2 = "(%+i days)" % round(offset)
+			if s.anim: o3 = "(animation on)"
+			else: o3 = ""
+			pygame.display.set_caption("%s  %s  %s" % (o1, o2, o3))
+		planets_dict = orrp.coordinates(ti[0], ti[1], ti[2], ti[3], ti[4])
+		xi, yi = pic2[0].get_size()
+		s.dazz.blit(pic2[0], (PL_CENTER[0] - xi//2, PL_CENTER[1] - yi//2))
+		for i, el in enumerate(planets_dict):
+			r = 55 * (i + 1) + 2
+			pygame.draw.circle(s.dazz, (80, 80, 80), (PL_CENTER[0], PL_CENTER[1]), r, width = 4)
+			feta = math.atan2(el[0], el[1])
+			coordinates = (r * math.sin(feta), r * math.cos(feta))
+			coordinates = (coordinates[0] + PL_CENTER[0], HEIGHT - (coordinates[1] + PL_CENTER[1]))
+			xi, yi = pic2[i + 1].get_size()
+			s.dazz.blit(pic2[i + 1], (coordinates[0] - xi//2, coordinates[1] - yi//2))
+
+		tres = min(s.res[0], s.res[1])
+		out = pygame.transform.smoothscale(s.dazz, (tres, tres))
+		s.screen.blit(out, (0, 0))
+		
+		pygame.display.flip()
+
+c = Orr()
+c.run()
+
